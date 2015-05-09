7b844a04b282a19e7bf623425af13a9ea7bfd1ac
rmuller17 <Xpl0sive>
2015-05-09 13:31:05 +0200

    formatting

--- src/components/CommentDelete.js
+++ src/components/CommentDelete.js
@@ -1,14 +1,11 @@
 var React = require("react");
 
 class CommentDelete extends React.Component {
-    render() {
-        return ( < div className = "commentCross"
-            id = {
-                this.props.id
-            }
-            onClick = {this.props.onCommentDelete}> X < /div>
-        )
-    }
+  render() {
+    return ( 
+    	<div className = "commentCross" id = {this.props.id} onClick = {this.props.onCommentDelete}> X < /div>
+    )
+  }
 }
 
 module.exports = CommentDelete;
--- src/components/CommentReplyList.js
+++ src/components/CommentReplyList.js
@@ -3,18 +3,17 @@ const Comment = require("./Comment");
 const CommentLikeSection = require("./CommentLikeSection");
 
 class CommentReplyList extends React.Component {
-
-    render() {
-        return (
-            <div className="replyList">
-                {this.props.replies ? <div>
-                        {this.props.replies.map((comment, index) => <Comment author={comment.author} id={index} key={index} onCommentDelete={this.props.onReplyDelete} replies={comment.replies}>
-                            {comment.text}
-                            <div><CommentLikeSection /></div>
-                        </Comment>)}
-                    </div> : null}
-            </div>
-        )
+  render() {
+      return (
+        <div className="replyList">
+            {this.props.replies ? <div>
+              {this.props.replies.map((comment, index) => <Comment author={comment.author} id={index} key={index} onCommentDelete={this.props.onReplyDelete} replies={comment.replies}>
+                {comment.text}
+                <div><CommentLikeSection /></div>
+            </Comment>)}
+          </div> : null}
+        </div>
+      )
     }
 };
 
