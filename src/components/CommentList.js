76c9c0facc8546eddf32abd87e3a32f36b86c985
rmuller17 <Xpl0sive>
2015-05-09 14:21:20 +0200

    formatting

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -10,26 +10,26 @@ class CommentList extends React.Component {
     render() {
         return (
             <div className="commentList">
-						{this.props.comments.map((comment,index) => 
-							<div>
-							<Comment 
-								onCommentDelete={this.props.onCommentDelete} 
+							{this.props.comments.map((comment,index) => 
+								<div>
+								<Comment 
+									onCommentDelete={this.props.onCommentDelete} 
+									key={index}
+									replies={comment.replies}
+									author={comment.author}> {comment.text}
+								<div>	
+								<CommentDelete id={index} onCommentDelete={this.props.onCommentDelete}/>
+								<CommentLikeSection />
+								<CommentReplySection
 								key={index}
+								id={index}
 								replies={comment.replies}
-								author={comment.author}> {comment.text}
-							<div>	
-							<CommentDelete id={index} onCommentDelete={this.props.onCommentDelete}/>
-							<CommentLikeSection />
-							<CommentReplySection
-							key={index}
-							id={index}
-							replies={comment.replies}
-							onCommentSubmit={this.props.onReplySubmit}
-							 hasReplies={comment.replies}/>
-							 </div>
-							 </Comment>
-							</div>
-								)}
+								onCommentSubmit={this.props.onReplySubmit}
+								 hasReplies={comment.replies} />
+								 </div>
+								 </Comment>
+								</div>
+									)}
 				</div>
         )
     };
